5dd0929723be8c2dd6577e1e0d22c4a3573abdfc
Swetha Suravajjula <125635019+SwethaSuravajjula@users.noreply.github.com>
2026-07-28 22:38:42 +0530

    Solve resnet-identity-block

--- resnet/resnet-identity-block/resnet-identity-block.py
+++ resnet/resnet-identity-block/resnet-identity-block.py
@@ -5,10 +5,8 @@ def identity_block(x, W1, W2):
     Returns: np.ndarray of shape (batch, channels) with identity residual block output
     """
     # YOUR CODE HERE
-    W1 = np.array(W1)
-    W2 = np.array(W2)
-    x_h = np.maximum(0,x @ W1.T)
-    y = np.maximum(((x_h @ W2.T) + x),0)
     
+    h = np.maximum(0,np.array(x) @ np.array(W1).T)
+    y = np.maximum(0, (h @ np.array(W2).T) + np.array(x))
     return y
     
