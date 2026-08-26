315372c7617102fbfc687a22fc09a967d390b967
Swetha Suravajjula <125635019+SwethaSuravajjula@users.noreply.github.com>
2026-08-26 16:48:06 +0530

    Solve word2vec-noise-dist

---
+++ word2vec/word2vec-noise-dist/word2vec-noise-dist.py
@@ -0,0 +1,11 @@
+import torch
+
+def noise_distribution(counts: torch.Tensor, alpha: float = 0.75) -> torch.Tensor:
+    """
+    Returns torch.Tensor of shape (vocab_size,), a probability distribution that sums to 1.
+    """
+    # YOUR CODE HERE
+    counts = counts.to(torch.float32)
+    probs = counts ** alpha / (counts ** alpha).sum()
+    return probs
+    
